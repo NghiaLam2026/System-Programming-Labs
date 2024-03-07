fc1dac8c36fb2cd9fadeacf8520ee723c2fa9de2
Nghia Lam <118234173+NghiaLam2026@users.noreply.github.com>
2024-03-07 17:32:35 -0500

    Add files via upload

---
+++ Lab 02/Lam_nghia_hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+char main(){
+    printf("\nHello World!\n"
+    "Welcome to the CSC 3320 System-Level Programming Course!\n"
+    "Our class is held every Tuesday and Thursday at 12:45pm\n"
+    "Our recitation session is held every [Day] at [Time]");
+    return 0;
+}
+
